15a162134f86f6899c72faed6e3c6a0c5c438a70
wangtieforcs <wangtie_913@outlook.com>
2026-05-26 15:50:07 -0600

    set up CORS for VUE frontend

--- app.py
+++ app.py
@@ -2,7 +2,11 @@ from flask import Flask
 from flask_cors import CORS
 
 app = Flask(__name__)
-CORS(app)
+CORS(
+    app,
+    origins=['http://localhost:5173', 'http://127.0.0.1:5173'],
+    supports_credentials=True,
+)
 
 @app.route('/')
 def hello_world():  # put application's code here
